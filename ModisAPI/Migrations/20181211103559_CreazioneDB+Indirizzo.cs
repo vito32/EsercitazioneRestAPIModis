b18f26dd8a667262368e44129ed7bea16ed7d9db
vito32 <32vito@gmail.com>
2018-12-11 11:43:16 +0100

    Insert "Indirizzo" Atribute

---
+++ ModisAPI/Migrations/20181211103559_CreazioneDB+Indirizzo.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ModisAPI.Migrations
+{
+    public partial class CreazioneDBIndirizzo : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Indirizzo",
+                table: "Studenti",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Indirizzo",
+                table: "Studenti");
+        }
+    }
+}
