831d00cf0ea85e16a87ed319d51ac89f5141eac0
vito32 <32vito@gmail.com>
2018-12-11 12:44:27 +0100

    Insert add new Studente

--- ModisAPI/WorkerServices/WorkerServiceStudenti.cs
+++ ModisAPI/WorkerServices/WorkerServiceStudenti.cs
@@ -11,23 +11,33 @@ namespace ModisAPI.WorkerServices
     {
         private ModisContext db;
 
-        public WorkerServiceSQLServerDB() {
+        public WorkerServiceSQLServerDB()
+        {
             db = new ModisContext();
-        }
-
-        public List<Studente> RestituisciListaStudenti()
-        {
-        return db.Studenti.ToList();
-    }
-
-        public Studente RestituisciStudente(int id)
-        {
-            return db.Studenti.Where(x => x.Id == id).FirstOrDefault();
         }
 
         public void CreaStudente(Studente studente)
         {
-            
+            db.Studenti.Add(studente);
+            db.SaveChanges();
+        }
+
+        public void ModificaStudente(Studente studenteModificato)
+        {
+            db.Studenti.Update(studenteModificato);
+            db.SaveChanges();
+        }
+
+        public List<Studente> RestituisciListaStudenti()
+        {
+            return db.Studenti.ToList();
+        }
+
+        public Studente RestituisciStudente(int id)
+        {
+
+            //return db.Studenti.Find(id); se siamo sicuri sia una chiave 
+            return db.Studenti.Where(x => x.Id == id).FirstOrDefault();
         }
     }
 
@@ -45,23 +55,23 @@ namespace ModisAPI.WorkerServices
     //    }
     //}
 
-    public class WorkerServiceStudenti : IWorkerServiceStudenti
-    {
-        public List<Studente> RestituisciListaStudenti()
-        {
-            var studente1 = new Studente { Id = 1, Cognome = "Mario", Nome = "Rossi" };
-            var studente2 = new Studente { Id = 2, Cognome = "MastroCesare", Nome = "Francesco" };
-            return new List<Studente> { studente1, studente2 };
-        }
+    //public class WorkerServiceStudenti : IWorkerServiceStudenti
+    //{
+    //    public List<Studente> RestituisciListaStudenti()
+    //    {
+    //        var studente1 = new Studente { Id = 1, Cognome = "Mario", Nome = "Rossi" };
+    //        var studente2 = new Studente { Id = 2, Cognome = "MastroCesare", Nome = "Francesco" };
+    //        return new List<Studente> { studente1, studente2 };
+    //    }
 
-        public Studente RestituisciStudente(int id)
-        {
-            return RestituisciListaStudenti().Where(x => x.Id == id).FirstOrDefault();
-        }
+    //    public Studente RestituisciStudente(int id)
+    //    {
+    //        return RestituisciListaStudenti().Where(x => x.Id == id).FirstOrDefault();
+    //    }
 
-        public void CreaStudente(Studente studente)
-        {
-            throw new NotImplementedException();
-        }
-    }
+    //    public void CreaStudente(Studente studente)
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+    //}
 }
