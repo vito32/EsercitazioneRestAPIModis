44ca53fe49a1942c741f058d8681c25bd00cfa8c
vito32 <32vito@gmail.com>
2018-12-12 17:05:31 +0100

    Insert ViewModelStudente

--- ModisAPI/WorkerServices/IWorkerServiceStudenti.cs
+++ ModisAPI/WorkerServices/IWorkerServiceStudenti.cs
@@ -5,7 +5,7 @@ namespace ModisAPI.WorkerServices
 {
     public interface IWorkerServiceStudenti
     {
-        List<Studente> RestituisciListaStudenti();
+        List<ViewModelStudente> RestituisciListaStudenti();
         Studente RestituisciStudente(int id);
         void CreaStudente(Studente studente);
         void ModificaStudente(Studente studenteModificato);
